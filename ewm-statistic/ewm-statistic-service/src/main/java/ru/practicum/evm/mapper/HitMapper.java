81f2cfc1f23e6330c8e31cb63abe3fc9ae272bca
Oksana_Makarova <79106047+OksanaVM@users.noreply.github.com>
2023-06-08 12:34:29 +0300

    основное но не рабочее

--- ewm-statistic/ewm-statistic-service/src/main/java/ru/practicum/evm/mapper/HitMapper.java
+++ ewm-statistic/ewm-statistic-service/src/main/java/ru/practicum/evm/mapper/HitMapper.java
@@ -2,8 +2,6 @@ package ru.practicum.evm.mapper;
 
 import lombok.experimental.UtilityClass;
 import ru.practicum.evm.dto.EndpointHitDto;
-import ru.practicum.evm.dto.HitDto;
-import ru.practicum.evm.dto.OutputHitDto;
 import ru.practicum.evm.model.EndpointHit;
 
 import java.time.LocalDateTime;
@@ -12,24 +10,6 @@ import static ru.practicum.evm.mapper.ViewStatMapper.formatter;
 
 @UtilityClass
 public class HitMapper {
-
-    public static OutputHitDto toCreatedHitDto(EndpointHit endpointHit) {
-        return new OutputHitDto(
-                endpointHit.getId(),
-                endpointHit.getApp(),
-                endpointHit.getUri(),
-                endpointHit.getIp(),
-                endpointHit.getTimestamp());
-    }
-
-    public static EndpointHit toHit(HitDto hitDto) {
-        EndpointHit endpointHit = new EndpointHit();
-        endpointHit.setApp(hitDto.getApp());
-        endpointHit.setUri(hitDto.getUri());
-        endpointHit.setIp(hitDto.getIp());
-        endpointHit.setTimestamp(hitDto.getTimestamp());
-        return endpointHit;
-    }
 
     public static EndpointHit toEndpointHit(EndpointHitDto endpointHitDto) {
         return EndpointHit.builder()
